ae8123d701e2caf805bccb44bdf17720b74e5dfc
Abhijith K <iamabhijith.k@gmail.com>
2025-05-09 03:00:01 +0530

    feat: Enhance StacInkWell with additional gesture handlers and properties
    Added support for onDoubleTap, onLongPress, onTapDown, onTapUp, onTapCancel, onSecondaryTap, onSecondaryTapUp, onSecondaryTapDown, onSecondaryTapCancel, onHighlightChanged, and onHover in the StacInkWell model.

--- packages/stac/lib/src/parsers/widgets/stac_inkwell/stac_inkwell_parser.dart
+++ packages/stac/lib/src/parsers/widgets/stac_inkwell/stac_inkwell_parser.dart
@@ -15,8 +15,64 @@ class StacInkwellParser extends StacParser<StacInkWell> {
   @override
   Widget parse(BuildContext context, StacInkWell model) {
     return InkWell(
-      onTap: () => Stac.onCallFromJson(model.onTap, context),
       child: Stac.fromJson(model.child, context),
+      onTap: model.onTap == null
+          ? null
+          : () => Stac.onCallFromJson(model.onTap, context),
+      onDoubleTap: model.onDoubleTap == null
+          ? null
+          : () => Stac.onCallFromJson(model.onDoubleTap, context),
+      onLongPress: model.onLongPress == null
+          ? null
+          : () => Stac.onCallFromJson(model.onLongPress, context),
+      onTapDown: model.onTapDown == null
+          ? null
+          : (_) => Stac.onCallFromJson(model.onTapDown, context),
+      onTapUp: model.onTapUp == null
+          ? null
+          : (_) => Stac.onCallFromJson(model.onTapUp, context),
+      onTapCancel: model.onTapCancel == null
+          ? null
+          : () => Stac.onCallFromJson(model.onTapCancel, context),
+      onSecondaryTap: model.onSecondaryTap == null
+          ? null
+          : () => Stac.onCallFromJson(model.onSecondaryTap, context),
+      onSecondaryTapUp: model.onSecondaryTapUp == null
+          ? null
+          : (_) => Stac.onCallFromJson(model.onSecondaryTapUp, context),
+      onSecondaryTapDown: model.onSecondaryTapDown == null
+          ? null
+          : (_) => Stac.onCallFromJson(model.onSecondaryTapDown, context),
+      onSecondaryTapCancel: model.onSecondaryTapCancel == null
+          ? null
+          : () => Stac.onCallFromJson(model.onSecondaryTapCancel, context),
+      onHighlightChanged: model.onHighlightChanged == null
+          ? null
+          : (_) => Stac.onCallFromJson(model.onHighlightChanged, context),
+      onHover: model.onHover == null
+          ? null
+          : (_) => Stac.onCallFromJson(model.onHover, context),
+      mouseCursor: model.mouseCursor?.value,
+      focusColor: model.focusColor.toColor(context),
+      hoverColor: model.hoverColor.toColor(context),
+      highlightColor: model.highlightColor.toColor(context),
+      overlayColor:
+          WidgetStateProperty.all(model.overlayColor?.toColor(context)),
+      splashColor: model.splashColor.toColor(context),
+      radius: model.radius,
+      borderRadius: model.borderRadius.parse,
+      customBorder: model.customBorder?.parse(context),
+      enableFeedback: model.enableFeedback,
+      excludeFromSemantics: model.excludeFromSemantics,
+      canRequestFocus: model.canRequestFocus,
+      onFocusChange: (_) => Stac.onCallFromJson(model.onFocusChange, context),
+      autofocus: model.autofocus,
+      hoverDuration: model.hoverDuration?.parse,
+
+      ///TODO : need to add support for
+      ///focusNode
+      ///splashFactory
+      ///statesController
     );
   }
 }
