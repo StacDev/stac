434bd4c50854f1fcdfebbd720815cc607e10d564
Akhil George <akhil8311@gmail.com>
2026-01-17 18:34:39 +0530

    feat: Add SliverPaddingParser to StacService

--- packages/stac/lib/src/framework/stac_service.dart
+++ packages/stac/lib/src/framework/stac_service.dart
@@ -114,6 +114,7 @@ class StacService {
     const StacRadioGroupParser(),
     const StacSliderParser(),
     const StacSliverAppBarParser(),
+    const StacSliverPaddingParser(),
     const StacOpacityParser(),
     const StacPlaceholderParser(),
     const StacAspectRatioParser(),
