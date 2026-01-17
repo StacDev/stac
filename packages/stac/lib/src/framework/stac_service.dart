a53a1ae8067f68aa1531e3580ed577520bda0ed2
Akhil George <akhil8311@gmail.com>
2026-01-17 19:48:27 +0530

    feat: Add SliverSafeArea widget and its parser with documentation and example

--- packages/stac/lib/src/framework/stac_service.dart
+++ packages/stac/lib/src/framework/stac_service.dart
@@ -114,6 +114,7 @@ class StacService {
     const StacRadioGroupParser(),
     const StacSliderParser(),
     const StacSliverAppBarParser(),
+    const StacSliverSafeAreaParser(),
     const StacOpacityParser(),
     const StacPlaceholderParser(),
     const StacAspectRatioParser(),
