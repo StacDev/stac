98c118e776588912ce54ec6aaf2887b3e456348a
Akhil George <akhil8311@gmail.com>
2026-01-27 16:35:30 +0530

    fix: Enable explicit JSON serialization for StacSliverPadding

--- packages/stac_core/lib/widgets/sliver_padding/stac_sliver_padding.dart
+++ packages/stac_core/lib/widgets/sliver_padding/stac_sliver_padding.dart
@@ -48,7 +48,7 @@ part 'stac_sliver_padding.g.dart';
 ///
 /// See also:
 ///  * Flutter's [SliverPadding documentation](https://api.flutter.dev/flutter/widgets/SliverPadding-class.html)
-@JsonSerializable()
+@JsonSerializable(explicitToJson: true)
 class StacSliverPadding extends StacWidget {
   /// Creates a [StacSliverPadding].
   const StacSliverPadding({required this.sliver, required this.padding});
