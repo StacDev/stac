b2129c00480b9a9b3100f769992067ac0fb71eef
Akhil George <akhil8311@gmail.com>
2026-01-28 12:11:56 +0530

    feat: add stacSliverList widget, parser, example, and documentation

--- packages/stac/lib/src/framework/stac_service.dart
+++ packages/stac/lib/src/framework/stac_service.dart
@@ -114,6 +114,7 @@ class StacService {
     const StacRadioGroupParser(),
     const StacSliderParser(),
     const StacSliverAppBarParser(),
+    const StacSliverListParser(),
     const StacSliverVisibilityParser(),
     const StacSliverOpacityParser(),
     const StacSliverSafeAreaParser(),
