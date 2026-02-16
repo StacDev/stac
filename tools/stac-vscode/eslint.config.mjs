c66f36167819e3622ae95fa3356a7809818e4e87
Divyanshu Bhargava <divyanshub024@gmail.com>
2026-02-16 11:52:51 +0530

    fix: update ESLint configuration for TypeScript files

--- tools/stac-vscode/eslint.config.mjs
+++ tools/stac-vscode/eslint.config.mjs
@@ -3,6 +3,7 @@ import typescriptEslint from "typescript-eslint";
 export default [{
     files: ["**/*.ts"],
 }, {
+    files: ["**/*.ts"],
     plugins: {
         "@typescript-eslint": typescriptEslint.plugin,
     },
